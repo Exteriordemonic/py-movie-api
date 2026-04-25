3e02fdaced96df4d632e68af8ea6ad6133db1fae
Kamil <kmirosz6@gmail.com>
2026-04-25 16:44:32 +0200

    Updated checklist with new guidelines and formatting improvements

---
+++ cinema/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from cinema.views import MoviesList, MovieDetail
+
+urlpatterns = [
+    path("movies/", MoviesList.as_view(), name="movies-list"),
+    path("movies/<int:pk>/", MovieDetail.as_view(), name="movie-detail"),
+    path("movies/<int:pk>/", MovieDetail.as_view(), name="movie-delete"),
+]
